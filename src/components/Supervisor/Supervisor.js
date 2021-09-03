cce788c249ea81045b40c378c3d9bfd433d88e0d
Havyner Caetano <hrcgeo@gmail.com>
2021-09-03 12:57:44 -0300

    feat: Create Supervisor component

--- src/components/Supervisor/Supervisor.js
+++ src/components/Supervisor/Supervisor.js
@@ -1,10 +1,45 @@
 import React from "react";
+import classNames from "classnames";
 import userPicture from "../../assets/Avatar.png";
+import styles from "./styles.module.css";
 
 export default function Supervisor() {
 	return (
-		<article>
-			<img src={userPicture} alt="User's profile picture" />
-		</article>
+		<main className={styles.body}>
+			<article className={styles.card}>
+				<img
+					className={styles.picture}
+					src={userPicture}
+					alt="User's profile picture"
+				/>
+				<h1 className={styles.name}>{"Sergio Martin"}</h1>
+				<div className={styles["content-wrapper"]}>
+					<p className={classNames(styles["card-text"], styles.label)}>Email</p>
+					<p
+						className={classNames(
+							styles["card-text"],
+							styles["user-information"]
+						)}
+					>
+						{"sergio.decas@martin.com"}
+					</p>
+				</div>
+				<div className={styles["content-wrapper"]}>
+					<p className={classNames(styles["card-text"], styles.label)}>
+						Mobile phone
+					</p>
+					<p
+						className={classNames(
+							styles["card-text"],
+							styles["user-information"]
+						)}
+					>
+						{"0456 456 456"}
+					</p>
+				</div>
+
+				<button className={styles.button}>Back to the list</button>
+			</article>
+		</main>
 	);
 }
