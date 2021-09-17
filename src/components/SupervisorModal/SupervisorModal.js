79b9fa863ad8da97b97f86641e675c7113748db5
Havyner Caetano <hrcgeo@gmail.com>
2021-09-17 11:18:35 -0300

    feat: Improve modal's style

--- src/components/SupervisorModal/SupervisorModal.js
+++ src/components/SupervisorModal/SupervisorModal.js
@@ -22,7 +22,12 @@ export default class SupervisorModal extends Component {
 		};
 
 		this.handleInputChange = this.handleInputChange.bind(this);
+		this.handleReturn = this.handleReturn.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	componentDidMount() {
+		Modal.setAppElement("body");
 	}
 
 	handleInputChange({ target: { name, value } }) {
@@ -30,6 +35,20 @@ export default class SupervisorModal extends Component {
 			...previousState,
 			[name]: value,
 		}));
+	}
+
+	handleReturn() {
+		this.props.handleCloseModal();
+		this.setState({
+			id: "",
+			firstName: "",
+			lastName: "",
+			state: "",
+			postcode: "",
+			employer: "",
+			phone: "",
+			email: "",
+		});
 	}
 
 	handleSubmit() {
@@ -42,7 +61,7 @@ export default class SupervisorModal extends Component {
 	}
 
 	render() {
-		const { isOpen, handleCloseModal } = this.props;
+		const { isOpen } = this.props;
 		const { firstName, lastName, state, postcode, employer, phone, email } =
 			this.state;
 
@@ -56,137 +75,143 @@ export default class SupervisorModal extends Component {
 					<h1 className={styles.title}>Add Supervisor</h1>
 					<hr className={styles.line} />
 					<form className={styles.form}>
-						<div
-							className={classNames(
-								styles["content-wrapper"],
-								styles["wrapper-first-name"]
-							)}
-						>
-							<label className={styles.label} htmlFor="first-name">
-								Name *
-							</label>
-							<input
-								className={styles.input}
-								type="text"
-								name="firstName"
-								placeholder="Add name"
-								value={firstName}
-								onChange={this.handleInputChange}
-							/>
+						<div className={styles["first-line"]}>
+							<div
+								className={classNames(
+									styles["content-wrapper"],
+									styles["wrapper-first-name"]
+								)}
+							>
+								<label className={styles.label} htmlFor="first-name">
+									Name *
+								</label>
+								<input
+									className={styles.input}
+									type="text"
+									name="firstName"
+									placeholder="Add name"
+									value={firstName}
+									onChange={this.handleInputChange}
+								/>
+							</div>
+							<div
+								className={classNames(
+									styles["content-wrapper"],
+									styles["wrapper-last-name"]
+								)}
+							>
+								<label className={styles.label} htmlFor="last-name">
+									Surname *
+								</label>
+								<input
+									className={styles.input}
+									type="text"
+									name="lastName"
+									placeholder="Add surname"
+									value={lastName}
+									onChange={this.handleInputChange}
+								/>
+							</div>
 						</div>
-						<div
-							className={classNames(
-								styles["content-wrapper"],
-								styles["wrapper-last-name"]
-							)}
-						>
-							<label className={styles.label} htmlFor="last-name">
-								Surname *
-							</label>
-							<input
-								className={styles.input}
-								type="text"
-								name="lastName"
-								placeholder="Add surname"
-								value={lastName}
-								onChange={this.handleInputChange}
-							/>
+						<div className={styles["second-line"]}>
+							<div
+								className={classNames(
+									styles["content-wrapper"],
+									styles["wrapper-state"]
+								)}
+							>
+								<label className={styles.label} htmlFor="state">
+									State *
+								</label>
+								<input
+									className={styles.input}
+									type="text"
+									name="state"
+									placeholder="i.e. NSW"
+									value={state}
+									onChange={this.handleInputChange}
+								/>
+							</div>
+							<div
+								className={classNames(
+									styles["content-wrapper"],
+									styles["wrapper-postcode"]
+								)}
+							>
+								<label className={styles.label} htmlFor="postcode">
+									Postcode *
+								</label>
+								<input
+									className={styles.input}
+									type="text"
+									name="postcode"
+									placeholder="i.e. 2000"
+									value={postcode}
+									onChange={this.handleInputChange}
+								/>
+							</div>
+							<div
+								className={classNames(
+									styles["content-wrapper"],
+									styles["wrapper-employer"]
+								)}
+							>
+								<label className={styles.label} htmlFor="employer">
+									Gym / studio *
+								</label>
+								<input
+									className={styles.input}
+									type="text"
+									name="employer"
+									placeholder="Add gym or studio"
+									value={employer}
+									onChange={this.handleInputChange}
+								/>
+							</div>
 						</div>
-						<div
-							className={classNames(
-								styles["content-wrapper"],
-								styles["wrapper-state"]
-							)}
-						>
-							<label className={styles.label} htmlFor="state">
-								State *
-							</label>
-							<input
-								className={styles.input}
-								type="text"
-								name="state"
-								placeholder="i.e. NSW"
-								value={state}
-								onChange={this.handleInputChange}
-							/>
+						<div className={styles["third-line"]}>
+							<div
+								className={classNames(
+									styles["content-wrapper"],
+									styles["wrapper-phone"]
+								)}
+							>
+								<label className={styles.label} htmlFor="phone">
+									Phone *
+								</label>
+								<input
+									className={styles.input}
+									type="text"
+									name="phone"
+									placeholder="0444 444 444"
+									value={phone}
+									onChange={this.handleInputChange}
+								/>
+							</div>
+							<div
+								className={classNames(
+									styles["content-wrapper"],
+									styles["wrapper-email"]
+								)}
+							>
+								<label className={styles.label} htmlFor="email">
+									Email *
+								</label>
+								<input
+									className={styles.input}
+									type="text"
+									name="email"
+									placeholder="Add email"
+									value={email}
+									onChange={this.handleInputChange}
+								/>
+							</div>
 						</div>
-						<div
-							className={classNames(
-								styles["content-wrapper"],
-								styles["wrapper-postcode"]
-							)}
-						>
-							<label className={styles.label} htmlFor="postcode">
-								Postcode *
-							</label>
-							<input
-								className={styles.input}
-								type="text"
-								name="postcode"
-								placeholder="i.e. 2000"
-								value={postcode}
-								onChange={this.handleInputChange}
-							/>
-						</div>
-						<div
-							className={classNames(
-								styles["content-wrapper"],
-								styles["wrapper-employer"]
-							)}
-						>
-							<label className={styles.label} htmlFor="employer">
-								Gym / studio *
-							</label>
-							<input
-								className={styles.input}
-								type="text"
-								name="employer"
-								placeholder="Add gym or studio"
-								value={employer}
-								onChange={this.handleInputChange}
-							/>
-						</div>
-						<div
-							className={classNames(
-								styles["content-wrapper"],
-								styles["wrapper-phone"]
-							)}
-						>
-							<label className={styles.label} htmlFor="phone">
-								Phone *
-							</label>
-							<input
-								className={styles.input}
-								type="text"
-								name="phone"
-								placeholder="0444 444 444"
-								value={phone}
-								onChange={this.handleInputChange}
-							/>
-						</div>
-						<div
-							className={classNames(
-								styles["content-wrapper"],
-								styles["wrapper-email"]
-							)}
-						>
-							<label className={styles.label} htmlFor="email">
-								Email *
-							</label>
-							<input
-								className={styles.input}
-								type="text"
-								name="email"
-								placeholder="Add email"
-								value={email}
-								onChange={this.handleInputChange}
-							/>
-						</div>
-						<div>
+						<div className={styles["container-buttons"]}>
 							<button
 								className={classNames(styles["button-return"], styles.button)}
 								type="button"
-								onClick={handleCloseModal}
+								onClick={this.handleReturn}
 							>
 								Back
 							</button>
